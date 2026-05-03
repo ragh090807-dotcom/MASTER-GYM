45ad8cd863839bd85d0e6a069fee783add5b0632
ragh090807-dotcom <ragh090807@gmail.com>
2026-05-03 22:50:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-function joinGym(e) {
-  e.preventDefault();
+function joinGym(event) {
+  event.preventDefault();
 
   const name = document.getElementById("name").value;
+  const phone = document.getElementById("phone").value;
   const plan = document.getElementById("plan").value;
 
   document.getElementById("message").innerText =
-    "Thank you " + name + "! You selected " + plan + " plan. We will contact you soon.";
+    "Thank you " + name + "! Your enquiry for the " + plan + " plan has been received. We will contact you soon.";
 
-  e.target.reset();
+  event.target.reset();
 }
