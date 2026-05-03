842366e88da6d863a1a180f2d45b08175d9eeae0
ragh090807-dotcom <ragh090807@gmail.com>
2026-05-03 22:19:03 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+function joinGym(event) {
+  event.preventDefault();
+
+  const name = document.getElementById("name").value;
+  const phone = document.getElementById("phone").value;
+  const plan = document.getElementById("plan").value;
+
+  document.getElementById("message").innerText =
+    "Thank you " + name + "! Your request for the " + plan + " plan has been received. We will contact you soon.";
+
+  event.target.reset();
+}
